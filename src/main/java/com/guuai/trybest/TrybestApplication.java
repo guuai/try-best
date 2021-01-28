9f83a267018d0b947dc4b89909d7473514a3b2de
jiangzhenyu <jiangzhenyu@csii.com.cn>
2021-01-28 11:01:28 +0800

    start

---
+++ src/main/java/com/guuai/trybest/TrybestApplication.java
@@ -0,0 +1,13 @@
+package com.guuai.trybest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrybestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrybestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/guuai/trybest/TrybestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guuai.trybest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrybestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
